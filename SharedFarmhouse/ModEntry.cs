37382e9852a241b6728a0609e31f98011785c29c
Caleb Stein <caleb@calebstein.net>
2024-05-28 15:02:00 -0700

    initial commit

---
+++ SharedFarmhouse/ModEntry.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using StardewModdingAPI;
+using StardewValley;
+
+namespace SharedFarmhouse;
+
+internal sealed class ModEntry : Mod
+{
+    public override void Entry(IModHelper helper)
+    {
+        
+    }
+}
