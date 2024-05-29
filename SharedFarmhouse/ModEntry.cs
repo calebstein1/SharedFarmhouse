64b3ca6bd79331b887c339c86cd1cde94a690e6b
Caleb Stein <caleb@calebstein.net>
2024-05-28 17:25:34 -0700

    prevent farmhand from getting deleted upon cabin demolition

--- SharedFarmhouse/ModEntry.cs
+++ SharedFarmhouse/ModEntry.cs
@@ -8,6 +8,16 @@ internal sealed class ModEntry : Mod
 {
     public override void Entry(IModHelper helper)
     {
-        
+        var harmony = new Harmony(ModManifest.UniqueID);
+
+        harmony.Patch(
+            original: AccessTools.Method(typeof(FarmerTeam), "DeleteFarmhand"),
+            prefix: new HarmonyMethod(typeof(ModEntry), nameof(DeleteFarmhandPrefix))
+        );
+    }
+
+    private static bool DeleteFarmhandPrefix()
+    {
+        return false;
     }
 }
