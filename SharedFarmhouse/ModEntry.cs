5bce2f3e726a6d464001f4f05b331a7583f8cadd
Caleb Stein <caleb@calebstein.net>
2024-05-30 17:07:17 -0700

    any player can check any mailbox

--- SharedFarmhouse/ModEntry.cs
+++ SharedFarmhouse/ModEntry.cs
@@ -1,6 +1,10 @@
-﻿using HarmonyLib;
+﻿using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
 using StardewModdingAPI;
 using StardewValley;
+using StardewValley.Network;
+using xTile.Dimensions;
 
 namespace SharedFarmhouse;
 
@@ -19,6 +23,11 @@ internal sealed class ModEntry : Mod
             original: AccessTools.Method(typeof(GameServer), nameof(GameServer.IsFarmhandAvailable)),
             postfix: new HarmonyMethod(typeof(ModEntry), nameof(IsFarmhandAvailablePostfix))
         );
+
+        harmony.Patch(
+            original: AccessTools.Method(typeof(GameLocation), nameof(GameLocation.performAction), new [] { typeof(string[]), typeof(Farmer), typeof(Location)}),
+            transpiler: new HarmonyMethod(typeof(ModEntry), nameof(FixMailboxTranspiler))
+        );
     }
 
     private static bool DeleteFarmhandPrefix()
@@ -29,5 +38,23 @@ internal sealed class ModEntry : Mod
     private static void IsFarmhandAvailablePostfix(ref bool __result)
     {
         __result = true;
+    }
+
+    private static IEnumerable<CodeInstruction> FixMailboxTranspiler(IEnumerable<CodeInstruction> instructions)
+    {
+        var output = new List<CodeInstruction>();
+        
+        foreach (var code in instructions)
+        {
+            if (code.opcode == OpCodes.Callvirt && (code.operand as MethodInfo)?.Name == "get_IsOwnedByCurrentPlayer")
+            {
+                output.RemoveAt(output.Count - 1);
+                output.Add(new CodeInstruction(OpCodes.Ldc_I4_1));
+                continue;
+            }
+            output.Add(code);
+        }
+
+        return output.AsEnumerable();
     }
 }
